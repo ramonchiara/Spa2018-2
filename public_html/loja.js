85198d8d9584856402f09b9aab33eae6f53f8c91
Prof. Ramon <ramon@ramon.pro.br>
2018-09-26 22:36:34 -0300

    Código da aula do dia 26/09/2018.

--- public_html/loja.js
+++ public_html/loja.js
@@ -5,12 +5,38 @@ $(document).ready(function () {
         var min = $('#preco-min').val();
         console.log(`preco-min: ${min}`);
         $('#valor-min').html(min);
+        listarProdutos();
     });
 
     $('#preco-max').on('input', function () {
         var max = $('#preco-max').val();
         console.log(`preco-max: ${max}`);
         $('#valor-max').html(max);
+        listarProdutos();
     });
+
+    $.ajax({
+        url: 'produtos.json',
+        method: 'GET',
+        success: function (data) {
+            console.log(data);
+            produtos = data;
+            listarProdutos();
+        }
+    });
+
+    var produtos = [];
+
+    function listarProdutos() {
+        var min = $('#preco-min').val();
+        var max = $('#preco-max').val();
+        $('#produtos').html('');
+        for (var i = 0; i < produtos.length; i++) {
+            var produto = produtos[i];
+            if (produto.preco >= min && produto.preco <= max) {
+                $('#produtos').append(`<li><a href="#${produto.id}">${produto.nome}</a> - R$ ${produto.preco}</li>`);
+            }
+        }
+    }
 
 });
