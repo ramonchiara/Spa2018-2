29ea73bcbf52c885f1b151acb02885322761b6e0
Ramon Chiara <ramonchiara@gmail.com>
2018-10-03 23:21:17 -0300

    Código da aula do dia 03/10/2018.

--- public_html/loja.js
+++ public_html/loja.js
@@ -2,34 +2,36 @@ $(document).ready(function () {
     console.log("Olá, mundo!");
 
     $('#preco-min').on('input', function () {
-        var min = $('#preco-min').val();
+        min = $('#preco-min').val();
         console.log(`preco-min: ${min}`);
         $('#valor-min').html(min);
         listarProdutos();
     });
 
     $('#preco-max').on('input', function () {
-        var max = $('#preco-max').val();
+        max = $('#preco-max').val();
         console.log(`preco-max: ${max}`);
         $('#valor-max').html(max);
         listarProdutos();
     });
 
-    $.ajax({
-        url: 'produtos.json',
-        method: 'GET',
-        success: function (data) {
-            console.log(data);
-            produtos = data;
-            listarProdutos();
-        }
-    });
+    function carregaProdutos() {
+        $.ajax({
+            url: 'produtos.json',
+            method: 'GET',
+            success: function (data) {
+                console.log(data);
+                produtos = data;
+                listarProdutos();
+            }
+        });
+    }
 
+    var min = 0;
+    var max = 10000;
     var produtos = [];
 
     function listarProdutos() {
-        var min = $('#preco-min').val();
-        var max = $('#preco-max').val();
         $('#produtos').html('');
         for (var i = 0; i < produtos.length; i++) {
             var produto = produtos[i];
@@ -41,5 +43,36 @@ $(document).ready(function () {
             $('#produtos').append('<li class="alert alert-warning" role="alert">Nenhum produto encontrado!</li>');
         }
     }
+
+    $(window).on('hashchange', function () {
+        var hash = window.location.hash;
+        var id = hash.substring(1);
+        console.log(hash + ' -->' + id);
+
+        if (id === '') {
+            $('#view').load('views/principal.html', function () {
+                carregaProdutos();
+            });
+        } else {
+            $('#view').load('views/produto.html', function () {
+                carregaProduto(id);
+            });
+        }
+    });
+
+    function carregaProduto(id) {
+        for (var i = 0; i < produtos.length; i++) {
+            var produto = produtos[i];
+            if (produto.id == id) {
+                $('#nome').html(produto.nome);
+                $('#preco').html(produto.preco);
+                break;
+            }
+        }
+    }
+
+    $('#view').load('views/principal.html', function () {
+        carregaProdutos();
+    });
 
 });
